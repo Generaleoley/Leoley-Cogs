d8c65277c5e3fd0ffbbc609378f32fb9603d9e67
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-04-16 20:21:35 -0600

    Update massping.py

--- massping/massping.py
+++ massping/massping.py
@@ -17,23 +17,8 @@ class massping:
         server = ctx.message.server
         host = ctx.message.author
     while loop == True:
-      await.bot.say("GET REKT {}".format(user))
-       ''' if loop > 200:
-            await self.bot.say ("Stupid kid... trying to mass ping someone over 200 times... well if you think you are SOOOOO clever... you're wrong...")
+        await.bot.say("GET REKT {}".format(user))
 
-        if user == host:
-            await self.bot.say("You're stupid, don't mass ping yourself")
-
-
-        if user == self.bot.user.id:
-            await self.bot.say ("WTF {}!!! DON'T MASS PING ME.".format(host))
-
-
-        while loop < count:
-            await self.bot.say("HAPPY APRIL FOOLS! {} ".format(user))
-            loop = loop + 1
-        else:
-            await self.bot.say ("{} Did you enjoy pinging {}, {} times?".format(host.mention, user, count)'''
 
 def setup(bot):
     bot.add.cog(massping(bot))
