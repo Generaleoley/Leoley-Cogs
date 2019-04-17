93b4120f91d8d7adac0f147ff070e3a65bf58e8e
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-04-17 07:20:45 -0600

    Update massping.py

--- massping/massping.py
+++ massping/massping.py
@@ -3,16 +3,25 @@ import asyncio
 import discord
 from discord.ext import commands
 
+loop = True
+
 class massping:
+
 
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
     async def massping(self, ctx, count: int, member: discord.Member=None,):
+        "Pings a user the "Count" amount of times.
         author = ctx.message.author
-        await self.bot.say("GET REKT {}".format(mention.member))
+
+        while loop == True:
+            await self.bot.say("GET REKT {}".format(member.mention))
+        else:
+            print ("exit")
 
 def setup(bot):
     bot.add_cog(massping(bot))
+
 
