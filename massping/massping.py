371b0f3d15c7a0d740f9d8477d3ca733f4de1d4a
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-04-16 20:36:55 -0600

    Update massping.py

--- massping/massping.py
+++ massping/massping.py
@@ -2,23 +2,19 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
-from __main__ import send_cmd_help
-
-loop = True
-
 
 class massping:
 
-    def __init__(self, bot):
+    def __init__(self,bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def massping(self, ctx, user: str, count: int):
-        server = ctx.message.server
-        host = ctx.message.author
-    while loop == True:
-        await self.bot.say("GET REKT {}".format(user))
-
+    async def massping(self, ctx, member: discord.Member=None,):
+        author = ctx.message.author
+        await self.bot.say("Get Rekt {}".format(author))
+        print ("1 complete")
+        await self.bot.say("GET REKT {}".format(member))
 
 def setup(bot):
     bot.add.cog(massping(bot))
+
