5f08719baee715f8816b5f7c672ea66e01f970ae
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-08-10 15:52:19 -0600

    Update massping.py

--- massping/massping.py
+++ massping/massping.py
@@ -9,7 +9,18 @@ class massping:
 
     def __init__(self,bot):
         self.bot = bot
-
+    
+    
+    @commands.command(pass_context=True, aliases = ["wehategeneral", "generaleoleysucks", "generaleoleydumb", "weakgeneral"])
+    async def diegeneraleoley(self, ctx):
+        author = ctx.message.author
+        await self.bot.say("You have been fined 100000 credits for your actions, please appeal your fine by DMing @Generaleoley#4249 with an apology:) ")
+        fine = int(100000)
+        bank = self.bot.get_cog("Economy").bank
+        bank.withdraw_credits(author, fine)
+        
+        
+        
     @commands.command(pass_context=True, aliases = ["welovegeneral", "generaleoleyop", "generaleoleyluv", "mightygeneral"])
     @commands.cooldown(rate=1, per=172800, type=commands.BucketType.user)
     async def hailgeneraleoley(self, ctx):
