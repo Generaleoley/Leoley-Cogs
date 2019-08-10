7af920b85a16a2f7bf23362efc29343c2cf8bb7d
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-08-10 14:04:31 -0600

    Update massping.py

--- massping/massping.py
+++ massping/massping.py
@@ -10,6 +10,16 @@ class massping:
     def __init__(self,bot):
         self.bot = bot
 
+    @commands.command(pass_context=True, aliases = ["welovegeneral", "generaleoleyop", "generaleoleyluv", "mightygeneral"])
+    @commands.cooldown(rate=1, per=172800, type=commands.BucketType.user)
+    async def hailgeneraleoley(self, ctx):
+        author = ctx.message.author
+        await self.bot.say("As a reward for showing your respect towards the great mighty generaleoley, you have been awarded 100 thousand credits!")
+        reward = int(100000)
+        bank = self.bot.get_cog("Economy").bank
+        bank.deposit_credits(author, reward)
+    
+    
     @commands.command(pass_context=True, aliases = ["mping"])
     async def massping(self, ctx, message: str, count: int, member: discord.Member=None,):
         author = ctx.message.author
@@ -22,6 +32,8 @@ class massping:
             mloop = mloop + 1
         else:
             await self.bot.say("Completed")
+            
+         
             
 
 def setup(bot):
